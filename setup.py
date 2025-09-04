e7cedd9d04e3015673f2d586ae04d258410bf5de
Victor-Garreau <victor.garreau@conscio-technologies.com>
2025-09-04 13:45:59 +0200

    FIX setup -> require pygame

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup(
     name="ViviEngine",
     version="1.0",
     packages=find_packages(),
+    install_requires=[
+        "pygame>=2.5.0",
+    ],
+    python_requires=">=3.12",
+    author="LeDarron",
+    author_email="",
+    description="A simple Python game engine",
+    license="MIT",
 )
